ab08bc2ec60b11dc9c0c38900fd88a5c7d6f318e
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-02-12 18:03:32 -0500

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css'
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      message: '',
+      messages: [
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+        "awdlkjalkdjlkwajd",
+      ],
+    }
+  }
+
+  sendMessage(e) {
+    e.preventDefault()
+
+    fetch('http://localhost:5000/send', {
+      method: 'POST',
+      body: JSON.stringify({
+        message: this.state.message
+      }),
+    }).then(console.log)
+
+    this.setState({
+      message: '',
+      messages: this.state.messages.concat(this.state.message)
+    })
+
+    this.lastMessageDom.scrollIntoView();
+  }
+
+  render() {
+    return (
+      <div className="chat-application">
+        <div className="chat-container">
+          <div className="chatbox">
+            <ul>
+              { this.state.messages.map(message => {
+                return <li>{message}</li>
+              })}
+              <li style={{ marginBottom: 30 }} ref={(el) => { this.lastMessageDom = el; }}></li>
+            </ul>
+          </div>
+          <div className="chat-input">
+            <form onSubmit={e => this.sendMessage(e)}>
+              <input
+                value={this.state.message}
+                onChange={e => this.setState({message: e.target.value})} type="text"
+              />
+            </form>
+          </div>
+        </div>
+        <div className="sidebar">
+          <ul className="user-list">
+            <li>max goyette</li>
+            <li>zergov</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
