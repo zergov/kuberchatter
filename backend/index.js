3183533207bd48446f93635aec7027d65863ebf5
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-02-17 00:26:30 -0500

    more stuff in /

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,11 @@
   const amqp = require('amqplib')
 
   // setup message queue
-  // const message_queue = 'kuberchatter_messages'
-  // const amqp_connection = await amqp.connect('amqp://localhost')
-  // const message_channel = await amqp_connection.createChannel()
-  // await message_channel.assertQueue(message_queue)
+  const message_queue = 'kuberchatter_messages'
+  const message_queue_host = process.env.RABBITMQ_SERVICE_HOST || 'localhost'
+  const amqp_connection = await amqp.connect(`amqp://${message_queue_host}`)
+  const message_channel = await amqp_connection.createChannel()
+  await message_channel.assertQueue(message_queue)
 
   const app = express()
 
@@ -19,7 +20,14 @@
   })
 
   // routes
-  app.get('/', (req, res) => res.json({message: 'Hello world!', envs: process.env}))
+  app.get('/', (req, res) => res.json({
+    message: 'Hello world!',
+    envs: process.env,
+    rabbit: {
+      connection: amqp_connection,
+      channel: message_channel,
+    }
+  }))
   // app.post('/send', async (req, res) => {
     // const {message} = req.body
     // message_channel.sendToQueue(message_queue, Buffer.from(message))
